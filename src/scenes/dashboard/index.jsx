b67a7cd75a5a0fbfa7f1ed91aa51e854a0cf7eac
eduardomaldonadofernandez21 <edumaldonadofdez21@gmail.com>
2024-04-02 10:29:52 +0100

    Creation contacts and team members

--- src/scenes/dashboard/index.jsx
+++ src/scenes/dashboard/index.jsx
@@ -1,9 +1,16 @@
-import React from 'react'
+import { Box, Button, IconButton, useTheme } from "@mui/material";
+import { tokens } from "../../theme";
+import Header from "../../components/Header";
 
 const Dashboard = () => {
+  const theme = useTheme();
+  const colors = tokens(theme.palette.mode);
+
   return (
-    <div>Dashboard</div>
+    <Box display="flex" justifyContent="space-between" alignItems="center">
+      <Header title="DASHBOARD" subtitle="Welcome to your dashborad"  />
+    </Box>
   );
-}
+};
 
 export default Dashboard;
